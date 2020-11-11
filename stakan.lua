1653fe2fcbc07b29f6cec7571df285857d77ce01
iafonkin <iafonkin@gmail.com>
2020-11-11 12:26:45 +0300

    Update stakan.lua
    доработан файл со стаканом

--- stakan.lua
+++ stakan.lua
@@ -1,6 +1,6 @@
 is_run = true
 let =""
-posicia = 200
+posicia = 50
 lag = 0
 
 
@@ -10,7 +10,7 @@ class_code = "SPBFUT"
 -- request_result = Subscribe_Level_II_Quotes(class_code, sec_code)
 
 z = getQuoteLevel2 (class_code, sec_code)
-z1 = z.bid_count
+z1 = z.offer_count
 
 
 
@@ -19,22 +19,26 @@ z1 = z.bid_count
 function OnQuote(class, sec ) 
 	if class == "SPBFUT" and sec == "HYZ0" then
 	z = getQuoteLevel2 (class, sec)
-	z1 = z.bid_count
-		for i=z1, 1, -1 do
-			if z.bid[i].quantity ~= nil then
-				lag = lag + tonumber(z.bid[i].quantity)
+    z1 = z.offer_count
+    z3 = 0
+    z4 = 0
+    lag = 0
+		for i=1, z1, 1 do
+			if z.offer[i].quantity ~= nil then
+				lag = lag + tonumber(z.offer[i].quantity)
 			
-				if lag > posicia then
-				z3 = tonumber(z.bid[i].price)
-				z4 = tonumber(z.bid[z.bid_count+0].price)
-				message("Full close :  " .. z3 .. " lag - ".. ( tonumber(z3-z4)))
-			
+				if lag > 50 and z3 == 0 then
+                    z3 = tonumber(z.offer[i].price)
+                    z4 = tonumber(z.offer[1].price)
+				    --z4 = tonumber(z.offer[z.offer_count+0].price) - так для половинке стакана  Покупки
+				    --message("Full close :  " .. z3 .. " prosadka po cene - ".. ( tonumber(z3-z4)))
 				end
 			end
 			
-								let = let .. "N-"..i.."  - price - ".. (z.bid[i].price)..";  "..(z.bid[i].quantity).."\n"
+								let = let .. "N-"..i.."  - price - ".. (z.offer[i].price)..";  "..(z.offer[i].quantity).."\n"
 		end
 								message (let)
+    message("Full close :  " .. z3 .. " prosadka po cene - ".. ( tonumber(z4-z3)))
 								let=""
 								lag = 0
 	end
